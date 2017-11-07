ef3efe30e7200f129563c629eb719f8021f71317
Instagram Map <igm@jpegdev.win>
2017-11-07 00:58:32 +0000

    Started authorization fixes

--- twim/lib/twim_web/controllers/page_controller.ex
+++ twim/lib/twim_web/controllers/page_controller.ex
@@ -3,23 +3,12 @@ defmodule TwimWeb.PageController do
 	require Logger
 
   def index(conn, _params) do
-		creds = OAuther.credentials(consumer_key: System.get_env("CONSUMER_KEY"), consumer_secret: System.get_env("CONSUMER_SECRET"),	token: System.get_env("TOKEN"), token_secret: System.get_env("TOKEN_SECRET"))
-		params = OAuther.sign("post", "https://api.twitter.com/1.1/statuses/lookup.json", [{"id", 485086311205048320}], creds)
-		{header, req_params} = OAuther.header(params)
-		{:ok, code, headers, ref}  = :hackney.post("https://api.twitter.com/1.1/statuses/lookup.json", [header], {:form, req_params})
-		{:ok, body} = :hackney.body(ref)
-		render conn, "index.html", body: Poison.encode!(Poison.Parser.parse!(body)) 
-  end
-
-	# Function courtesy of https://www.codesd.com/item/recursively-convert-the-map-to-the-list-of-keywords.html
-	def to_keyword_list(map) do
-    Enum.map(map, fn {k,v} ->
-      v = cond do
-        is_map(v) -> to_keyword_list(v)
-        true      -> v
-      end
-
-      {String.to_atom("#{k}"), v}
-    end)
+	url = "https://api.twitter.com/oauth/request_token"	
+	creds = OAuther.credentials(consumer_key: System.get_env("CONSUMER_KEY"), consumer_secret: System.get_env("CONSUMER_SECRET"),	token: System.get_env("TOKEN"), token_secret: System.get_env("TOKEN_SECRET"))
+	params = OAuther.sign("post", url, [], creds)
+	{header, req_params} = OAuther.header(params)
+	{:ok, code, header, ref} = :hackney.request('post', url, [header], {:form, req_params})
+    	Logger.debug "#{inspect(:hackney.body(ref))}"
+	render conn, "index.html"#, body: Poison.encode!(Poison.Parser.parse!(body)) 
   end
 end
